cb15a691ccd894c106be222a5ec6bc2f5c59b0a7
zimrecording <164475700+zimrecording@users.noreply.github.com>
2024-03-22 16:15:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 def draftingemails(email, openai_api_key):
     url = "https://api.openai.com/v1/chat/completions"
-    # model_name = "ft:gpt-3.5-turbo-1106:personal:email2:95ARg6mi"
-    model_name = "gpt-3.5-turbo"
+    model_name = "ft:gpt-3.5-turbo-1106:personal:email2:95ARg6mi"
+    
     headers = {
         "Authorization": f"Bearer {openai_api_key}"
     }
